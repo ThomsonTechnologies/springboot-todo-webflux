1e3e6df685e19514c78d2767d5c15be0c68a23c1
thomson-mathew <thomsonscode@gmail.com>
2019-02-19 01:30:16 -0600

    Created the project
    Created the project

---
+++ src/main/java/com/thomsoncodes/todo/SpringBootTodoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.thomsoncodes.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodoWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomsoncodes/todo/SpringBootTodoWebfluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thomsoncodes.todo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTodoWebfluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
